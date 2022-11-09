0c1c2763dce1f550b767a71dc49d95fc5fe0c906
Marco Aurélio <marcoaureliofm26@gmail.com>
2022-11-09 09:30:50 -0300

    p2p.v1

---
+++ src/com/za/networking/peertopeer/ServerThreadThread.java
@@ -0,0 +1,30 @@
+package com.za.networking.peertopeer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class ServerThreadThread extends Thread {
+    private ServerThread serverThread;
+    private Socket socket;
+    private PrintWriter printWriter;
+
+    public ServerThreadThread(Socket socket, ServerThread serverThread) {
+        this.serverThread = serverThread;
+        this.socket = socket;
+    }
+
+    public void  run(){
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(this.socket.getInputStream()
+            ));
+        } catch (Exception e) {
+            serverThread.getServerThreadThreads().remove(this);
+        }
+    }
+    public PrintWriter getPrintWriter() {
+        return printWriter;
+    }
+}
